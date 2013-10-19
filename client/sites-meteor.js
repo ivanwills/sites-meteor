ab2b23dd80e43a859787f748ce4eb353906352e4
Ivan Wills <ivan.wills@gmail.com>
2013-10-19 14:12:01 +1100

    Removed commented out code

--- client/sites-meteor.js
+++ client/sites-meteor.js
@@ -49,9 +49,6 @@ Template.filter.selected = Template.filter_option.selected = function (all) {
 
 Template.filter.filters = function () {
     var filter = db.config.findOne({"name" : "filters" });
-    //if ( !ReactiveLocal.get("filter") || !filter || !filter.value || !filter.value.length ) {
-        //ReactiveLocal.set("filter", "All" );
-    //}
     return filter && filter.value ? filter.value : false;
 };
 
